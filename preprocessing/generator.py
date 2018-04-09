c72aefab766dc7f2c5aeab5693fa57e7bf43b850
Kyle Yoon <apps.yoon@gmail.com>
2018-04-09 18:15:19 -0500

    Added validation, drop out layer, and training history

--- preprocessing/generator.py
+++ preprocessing/generator.py
@@ -10,69 +10,88 @@ from keras.preprocessing.image import ImageDataGenerator
 TRAIN_PATH = './data/stage1_train/'
 TEST_PATH = './data/stage1_test/'
 
-def preprocess_train(input_size):
-	#data/
-	#    stage1_train/
-	#         image_id/
-	#             images/
-	#             masks/
-	#         image_id/
-	# ...
-	print('Creating training generator...')
 
-	# TODO: Pass in the save path via arg
-	if os.path.isfile('./data/train_X.npy') is True and os.path.isfile('./data/train_Y.npy') is True:
-		print('Loading previously processed training data...')
-		# try to load the formatted training data
-		train_X = np.load('./data/train_X.npy')
-		train_Y = np.load('./data/train_Y.npy')
-	else:
-		print('Processing training data...')
-		train_ids = next(os.walk(TRAIN_PATH))[1] # getting the name of the images
+def preprocess_train(input_size, val_split=0.1):
+    # data/
+    #    stage1_train/
+    #         image_id/
+    #             images/
+    #             masks/
+    #         image_id/
+    # ...
+    print('Creating training generator...')
 
-		train_X = np.zeros((len(train_ids), input_size[0], input_size[1], input_size[2]), dtype=np.uint8) # width, height, channels
-		train_Y = np.zeros((len(train_ids), input_size[0], input_size[1], 1), dtype=np.bool)
+    # TODO: Pass in the save path via arg
+    if os.path.isfile('./data/train_X.npy') is True and os.path.isfile('./data/train_Y.npy') is True:
+        print('Loading previously processed training data...')
+        # try to load the formatted training data
+        train_X = np.load('./data/train_X.npy')
+        train_Y = np.load('./data/train_Y.npy')
+    else:
+        print('Processing training data...')
+        # getting the name of the images
+        train_ids = next(os.walk(TRAIN_PATH))[1]
 
-		for index_, image_id in tqdm(enumerate(train_ids), total=len(train_ids)):
-		    # base path
-		    path = TRAIN_PATH + image_id
-		    # getting the images
-		    image_path = path + '/images/' + image_id + '.png'
-		    image = imread(image_path)
-		    resized = resize(image, (input_size[0], input_size[1], input_size[2]), mode='constant', preserve_range=True)
-		    train_X[index_] = resized
-		    # getting the masks
-		    complete_mask = np.zeros((input_size[0], input_size[1], 1), dtype=np.bool)
-		    for mask_id in next(os.walk(path + '/masks/'))[2]:
-		        mask_path = path + '/masks/' + mask_id
-		        mask = imread(mask_path)
-		        resized_mask = resize(mask, (input_size[0], input_size[1], 1), mode='constant', preserve_range=True)
-		        # creating one mask for all the masks for this image
-		        complete_mask = np.maximum(resized_mask, complete_mask)
-		    train_Y[index_] = complete_mask
+        np.random.shuffle(train_ids)
 
-	    # save the formatted training data
-		np.save('./data/train_X', train_X)
-		np.save('./data/train_Y', train_Y)
+        # width, height, channels
+        train_X = np.zeros(
+            (len(train_ids), input_size[0], input_size[1], input_size[2]), dtype=np.uint8)
+        train_Y = np.zeros(
+            (len(train_ids), input_size[0], input_size[1], 1), dtype=np.bool)
 
-	return (train_X, train_Y)
+        for index_, image_id in tqdm(enumerate(train_ids), total=len(train_ids)):
+            # base path
+            path = TRAIN_PATH + image_id
+            # getting the images
+            image_path = path + '/images/' + image_id + '.png'
+            image = imread(image_path)
+            resized = resize(
+                image, (input_size[0], input_size[1], input_size[2]), mode='constant', preserve_range=True)
+            train_X[index_] = resized
+            # getting the masks
+            complete_mask = np.zeros(
+                (input_size[0], input_size[1], 1), dtype=np.bool)
+            for mask_id in next(os.walk(path + '/masks/'))[2]:
+                mask_path = path + '/masks/' + mask_id
+                mask = imread(mask_path)
+                resized_mask = resize(
+                    mask, (input_size[0], input_size[1], 1), mode='constant', preserve_range=True)
+                # creating one mask for all the masks for this image
+                complete_mask = np.maximum(resized_mask, complete_mask)
+            train_Y[index_] = complete_mask
+
+    # save the formatted training data
+        np.save('./data/train_X', train_X)
+        np.save('./data/train_Y', train_Y)
+
+    val_size = int(len(train_X) * val_split)
+
+    return (train_X[val_size:], train_Y[val_size:], train_X[:val_size], train_Y[:val_size])
 
 
 def preprocess_test(input_size):
-	test_ids = next(os.walk(TEST_PATH))[1]
+    test_ids = next(os.walk(TEST_PATH))[1]
 
-	test_X = np.zeros((len(test_ids), input_size[0], input_size[1], input_size[2]), dtype=np.uint8)
-	test_image_sizes = [] # we are going to resize the predicted test images back to original size
+    test_X = np.zeros(
+        (len(test_ids), input_size[0], input_size[1], input_size[2]), dtype=np.uint8)
+    # we are going to resize the predicted test images back to original size
+    test_image_sizes = []
 
-	for index_, test_id in tqdm(enumerate(test_ids), total=len(test_ids)):
-	    image_path = TEST_PATH + test_id + '/images/' + test_id + '.png'
-	    image = imread(image_path)
-	    test_image_sizes.append((image.shape[0], image.shape[1]))
-	    resized = resize(image, (input_size[0], input_size[1], input_size[2]), mode='constant', preserve_range=True)
-	    test_X[index_] = resized
+    for index_, test_id in tqdm(enumerate(test_ids), total=len(test_ids)):
+        image_path = TEST_PATH + test_id + '/images/' + test_id + '.png'
+        image = imread(image_path)
+        test_image_sizes.append((image.shape[0], image.shape[1]))
+        resized = resize(
+            image, (input_size[0], input_size[1], input_size[2]), mode='constant', preserve_range=True)
+        test_X[index_] = resized
 
-	return (test_X, test_ids, test_image_sizes)
+    return (test_X, test_ids, test_image_sizes)
 
 
 def create_train_generator():
-	return ImageDataGenerator(rotation_range=45, width_shift_range=0.25, height_shift_range=0.25, horizontal_flip=True, vertical_flip=True)
+    return ImageDataGenerator(rotation_range=45, width_shift_range=0.25, height_shift_range=0.25, horizontal_flip=True, vertical_flip=True)
+
+
+def create_val_generator():
+    return ImageDataGenerator()
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import random
 import numpy as np
 import pandas as pd
+import pickle
 from tqdm import tqdm
 
 from skimage.transform import resize
@@ -13,35 +14,40 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.optimizers import Adam
 
 from keras_unet.unet import unet
-from preprocessing.generator import preprocess_train, preprocess_test, create_train_generator
+from preprocessing.generator import preprocess_train, preprocess_test, create_train_generator, create_val_generator
 
 
 IMG_WIDTH = IMG_HEIGHT = 256
 IMG_CHANNELS = 3
 
-NUM_EPOCHS = 30
-STEPS_PER_EPOCH = 600 # there are 670 images
+NUM_EPOCHS = 1
+STEPS_PER_EPOCH = 10  # there are 670 images
 
 
 def create_model():
-	print('Creating model...')
+    print('Creating model...')
 
-	model = unet(input=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-	optimizer = Adam(lr=1e-5)
+    model = unet(input=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    optimizer = Adam(lr=1e-5)
 
-	model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=[]) # TODO: Metrics
+    model.compile(optimizer=optimizer, loss='binary_crossentropy',
+                  metrics=[])  # TODO: Metrics
 
-	model.summary()
+    model.summary()
 
-	return model
+    return model
 
 
 def create_callbacks():
-	print('Creating callbacks...')
-	early = EarlyStopping(patience=3, verbose=1)
-	checkpoint = ModelCheckpoint('./snapshots/keras_unet.h5', verbose=1, save_best_only=True)
+    print('Creating callbacks...')
+    checkpoint = ModelCheckpoint(
+        './snapshots/keras_unet_{{epoch:02d}}.h5'.format(), verbose=1)
 
-	return [early, checkpoint]
+    return [checkpoint]
+
+
+def create_generators():
+    return create_train_generator(), create_val_generator()
 
 
 def rle_encoding(x):
@@ -49,14 +55,16 @@ def rle_encoding(x):
     x: numpy array of shape (height, width), 1 - mask, 0 - background
     Returns run length as list
     '''
-    dots = np.where(x.T.flatten() == 1)[0] # .T sets Fortran order down-then-right
+    dots = np.where(x.T.flatten() == 1)[
+        0]  # .T sets Fortran order down-then-right
     run_lengths = []
     prev = -2
     for b in dots:
-        if (b > prev + 1): run_lengths.extend((b + 1, 0))
+        if (b > prev + 1):
+            run_lengths.extend((b + 1, 0))
         run_lengths[-1] += 1
         prev = b
-        
+
     return run_lengths
 
 
@@ -67,55 +75,72 @@ def prob_to_rles(x, cutoff=0.5):
 
 
 def main():
-	# preprocess data 
-	train_X, train_Y = preprocess_train(input_size=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
+    # preprocess data
+    train_X, train_Y, val_X, val_Y = preprocess_train(
+        input_size=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
 
-	# create generators
-	train_generator = create_train_generator()
-	train_generator.fit(train_X)
+    # create generators
+    train_generator, val_generator = create_generators()
+    train_generator.fit(train_X)
+    val_generator.fit(val_X)
 
-	# create the model
-	model = create_model()
+    # create the model
+    model = create_model()
 
-	# create the callbacks
-	callbacks = create_callbacks()
+    # create the callbacks
+    callbacks = create_callbacks()
 
-	if os.path.isfile('./snapshots/keras_unet.h5') is True:
-		print('loading model...')
-		model = load_model('./snapshots/keras_unet.h5')
-	else:
-		# train
-		print('Starting training...')
-		model.fit_generator(train_generator.flow(train_X, train_Y), epochs=NUM_EPOCHS, steps_per_epoch=STEPS_PER_EPOCH, callbacks=callbacks, verbose=1)
-		print('Finished training...')
-		model.save('./snapshots/keras_unet.h5')
+    if os.path.isfile('./snapshots/keras_unet.h5') is True:
+        print('loading model...')
+        model = load_model('./snapshots/keras_unet.h5')
+    else:
+        # train
+        print('Starting training...')
+        history = model.fit_generator(train_generator.flow(train_X, train_Y, batch_size=1),
+                                      epochs=NUM_EPOCHS,
+                                      steps_per_epoch=STEPS_PER_EPOCH,
+                                      callbacks=callbacks,
+                                      verbose=1,
+                                      validation_data=val_generator.flow(
+                                          val_X, val_Y, batch_size=1),
+                                      validation_steps=STEPS_PER_EPOCH * 0.1)
 
-	print('Starting submission...')
+        print('Finished training...')
+        # save model
+        model.save('./snapshots/keras_unet.h5')
+        # save training history
+        with open('history.pkl', 'wb') as history_file:
+            pickle.dump(history.history, history_file)
 
-	test_X, test_ids, test_image_sizes = preprocess_test(input_size=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
+    print('Starting submission...')
 
-	predictions = model.predict(test_X, verbose=1)
+    test_X, test_ids, test_image_sizes = preprocess_test(
+        input_size=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS))
 
-	preds = np.squeeze(predictions)
+    predictions = model.predict(test_X, verbose=1)
 
-	# resizing the predictions to original size
-	preds_resized = []
-	for index_, pred in enumerate(preds):
-	    image = resize(pred, test_image_sizes[index_], mode='constant', preserve_range=True)
-	    preds_resized.append(image)
+    preds = np.squeeze(predictions)
 
-	new_test_ids = []
-	rles = []
-	for n, id_ in enumerate(test_ids):
-	    rle = list(prob_to_rles(preds_resized[n]))
-	    rles.extend(rle)
-	    new_test_ids.extend([id_] * len(rle))
+    # resizing the predictions to original size
+    preds_resized = []
+    for index_, pred in enumerate(preds):
+        image = resize(
+            pred, test_image_sizes[index_], mode='constant', preserve_range=True)
+        preds_resized.append(image)
 
-	submission = pd.DataFrame()
-	submission['ImageId'] = new_test_ids
-	submission['EncodedPixels'] = pd.Series(rles).apply(lambda x: ' '.join(str(y) for y in x))
-	submission.to_csv('keras_unet.csv', index=False)
+    new_test_ids = []
+    rles = []
+    for n, id_ in enumerate(test_ids):
+        rle = list(prob_to_rles(preds_resized[n]))
+        rles.extend(rle)
+        new_test_ids.extend([id_] * len(rle))
+
+    submission = pd.DataFrame()
+    submission['ImageId'] = new_test_ids
+    submission['EncodedPixels'] = pd.Series(rles).apply(
+        lambda x: ' '.join(str(y) for y in x))
+    submission.to_csv('keras_unet.csv', index=False)
 
 
 if __name__ == '__main__':
-	main()
+    main()
